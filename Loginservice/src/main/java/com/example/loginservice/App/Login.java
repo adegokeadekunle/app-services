9beea4764e325c385a9486c9bad288c88bc91c0e
peaceonyinye <onyinyechipresh05@gmail.com>
2022-12-01 23:24:46 +0100

    second commit

--- Loginservice/src/main/java/com/example/loginservice/App/Login.java
+++ Loginservice/src/main/java/com/example/loginservice/App/Login.java
@@ -1,8 +1,6 @@
 package com.example.loginservice.App;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,6 +13,7 @@ import lombok.NoArgsConstructor;
 @Builder
 public class Login {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     Long id;
     String name;
     @Column(nullable = false,
